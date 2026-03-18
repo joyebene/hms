f1d80db98b4c3bea72d55c96e213d8431940ed26
Jaymax-cyber <jossycode0@gmail.com>
2026-03-18 16:31:37 +0100

    updt

--- app/api/utils/role.ts
+++
@@ -1,9 +0,0 @@
-
-interface User {
-  role: "ADMIN" | "DOCTOR" | "NURSE" | "RECEPTIONIST";
-}
-export function requireRole(user: User, roles: string[]) {
-  if (!roles.includes(user.role)) {
-    throw new Error("Forbidden");
-  }
-}
